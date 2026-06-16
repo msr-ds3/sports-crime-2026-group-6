40060c72876fd2a591f10a0705f3dfd9f824c509
ChanaRapp <crapp1@mail.yu.edu>
2026-06-16 14:49:54 -0400

    Match teams to cities

--- scripts/01_get_schedules.R
+++ scripts/01_get_schedules.R
@@ -1,13 +1,9 @@
 library(tidyverse)
 library(cfbfastR)
-library(tidyverse)
+
 Sys.setenv(CFBD_API_KEY = "QBNFmU8m5+31jgTj9mFW/X663VAAFG5hPyJWRFkBefobSTDoxskqxztPhyL434KL")
 
-<<<<<<< HEAD
-View(slice(cfbd_pbp_data(year = 2001), 1:25))
-=======
 years <- 2000:2005
->>>>>>> c36d1c603b908beddf619b4c275b71e2970f5897
 
 games <- map_dfr(years, function(yr) {
   cfbd_game_info(year = yr)
