3d8eeac358c06a8d8f7235d68b912ace66cf66ae
ChanaRapp <crapp1@mail.yu.edu>
2026-06-16 14:07:32 -0400

    Edits

--- scripts/01_get_schedules.R
+++ scripts/01_get_schedules.R
@@ -1,5 +1,6 @@
+library(tidyverse)
 library(cfbfastR)
 Sys.setenv(CFBD_API_KEY = "QBNFmU8m5+31jgTj9mFW/X663VAAFG5hPyJWRFkBefobSTDoxskqxztPhyL434KL")
 
-cfbd_pbp_data(year = 2001)
+View(slice(cfbd_pbp_data(year = 2001), 1:25))
 
---
+++ scripts/02_filter_nibrs.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+View(slice(read_csv("C:/Users/chana/Documents/final_project/sports-crime-2026-group-6/Data/nibrs_offense_segment_2001.csv"), 1:25))
+View(slice(read_csv("C:/Users/chana/Documents/final_project/sports-crime-2026-group-6/Data/nibrs_batch_header_1991_2024.csv"), 1:25))
