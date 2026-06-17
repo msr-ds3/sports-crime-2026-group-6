561bd3ea5c03728129f442e503123d7f740b758b
ChanaRapp <crapp1@mail.yu.edu>
2026-06-17 12:34:13 -0400

    Completed table 2

--- scripts/team_by_city.R
+++ scripts/team_by_city.R
@@ -19,15 +19,47 @@ cities_and_ori <-
   mutate(city = city_name, state = str_sub(ori, 1, 2)) %>%
   select(ori, state, city)
 
-cities <- c("Akron", "Ames", "Ann Arbor", "Athens", "Austin", "Blacksburg", "Boise", "Clemson", 
-"Colorado Springs", "Columbia", "Columbus", "Denton", "East Lansing", "Fayetteville", "Huntington",
-"Iowa City", "Jonesboro", "Kalamazoo", "Lawrence", "Logan", "Lubbock", "Morgantown", "Moscow",
-"Mount Pleasant", "Murfreesboro", "Provo")
+team_city <- tribble(
+  ~team, ~city_name, ~state_abbreviation,
+  "Akron", "akron", "oh",
+  "Iowa State", "ames", "ia",
+  "Michigan", "ann arbor", "mi",
+  "Ohio", "athens", "oh",
+  "Texas", "austin", "tx",
+  "Virginia Tech", "blacksburg", "va",
+  "Boise State", "boise", "id",
+  "Clemson", "clemson", "sc",
+  "Air Force", "colorado springs", "co",
+  "South Carolina", "columbia", "sc",
+  "Ohio State", "columbus", "oh",
+  "North Texas", "denton", "tx",
+  "Michigan State", "east lansing", "mi",
+  "Arkansas", "fayetteville", "ar",
+  "Marshall", "huntington", "wv",
+  "Iowa", "iowa city", "ia",
+  "Arkansas State", "jonesboro", "ar",
+  "Western Michigan", "kalamazoo", "mi",
+  "Kansas", "lawrence", "ks",
+  "Utah State", "logan", "ut",
+  "Texas Tech", "lubbock", "tx",
+  "West Virginia", "morgantown", "wv",
+  "Idaho", "moscow", "id",
+  "Eastern Michigan", "mount pleasant", "mi",
+  "Middle Tennessee", "murfreesboro", "tn",
+  "BYU", "provo", "ut"
+)
+
+relevant_cities_with_ori <-
+  cities_and_ori %>%
+  filter(city %in% str_to_lower(team_city$city_name) & state %in% str_to_upper(team_city$state_abbreviation)) %>%
+  select(city, state, ori) %>%
+  distinct() %>%
+  group_by(city)
 
 games_with_city_and_ori <-
-  inner_join(games_by_city, cities_and_ori, by = c("city", "state")) %>%
-  filter(city %in% str_to_lower(cities)) %>%
+  inner_join(games_by_city, relevant_cities_with_ori, by = c("city", "state")) %>%
+  select(game_date, team, city, state, ori) %>%
   distinct()
 
-
 write_csv(games_with_city_and_ori, "data/processed/games_per_city_with_ori.csv")
+write_csv(relevant_cities_with_ori, "data/processed/city_to_ori.csv")
